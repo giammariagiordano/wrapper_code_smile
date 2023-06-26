47ccf7167cd14747f0cf4a5c8829e486c015b69c
Giammaria Giordano <32549979+giammariagiordano@users.noreply.github.com>
2023-06-26 16:16:15 +0200

    fix get_commits_between_two_releases

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import subprocess
 
 import pandas as pd
@@ -10,7 +9,7 @@ from pydriller import Repository
 def get_all_directory_projects(project_path):
     projects = []
     for dir in os.listdir(project_path):
-        if os.path.isdir(project_path + dir):
+        if dir != ".DS_Store" and os.path.isdir(os.path.join(project_path, dir)):
             projects.append(dir)
     return projects
 
@@ -18,15 +17,6 @@ def get_all_directory_projects(project_path):
 def get_list_of_releases(repo):
     releases = []
     for commit in Repository(repo, only_releases=True).traverse_commits():
-        print(
-            'The commit {} has been modified by {}, '
-            'committed by {} in date {}'.format(
-                commit.hash,
-                commit.author.name,
-                commit.committer.name,
-                commit.committer_date
-            )
-        )
         releases.append(commit)
     return releases
 
@@ -50,16 +40,20 @@ def get_list_of_commits_between_two_releases_with_hash(repo, initial_release, fi
 
 
 def checkout_to_commit(project, commit):
-    repo = git.Repo(project)
-    repo.git.checkout(commit.hash)
-    print("Checkout to commit " + commit.hash + " successfully")
+    try:
+        repo = git.Repo(project)
+        repo.git.checkout(commit.hash)
+        print("Checkout to commit " + commit.hash + " successfully")
+        return True
+
+    except:
+        print("Error checking out commit " + commit.hash)
+        return False
 
 
 def run_code_smile(project, output_path):
-    print(output_path)
     if not os.path.exists(output_path):
         os.makedirs(output_path)
-
     output_path = os.path.abspath(output_path) + "/"
     project = project + "/"
     command = ["python", "/Users/broke31/PycharmProjects/wrapper_code_smile/code_smile/controller/analyzer.py",
@@ -68,48 +62,51 @@ def run_code_smile(project, output_path):
     print("Code_smile executed successfully")
 
 
-def analyze_commit_by_commit(project, commit_i, commit_j):
-    commits = get_list_of_commits_between_two_releases(project, commit_i, commit_j)
+def analyze_commit_by_commit(path_with_releases, commit_i, commit_j, project, base_dir):
+    path_without_release = os.path.dirname(path_with_releases)
+    commit_i_without_proj_name = get_hash_from_project_name(commit_i)
+    commit_j_without_proj_name = get_hash_from_project_name(commit_j)
+    commits = get_list_of_commits_between_two_releases(base_dir + project, commit_i_without_proj_name,
+                                                       commit_j_without_proj_name)
+    project += "/"
     for commit in commits:
-        new_project_name = project + "internal_commit" + "___" + str(
-            commit.hash) + "___between_commits" + commit_i + "___" + commit_j
-
-        os.rename(project, new_project_name)
-
-        checkout_to_commit(
-            project + "internal_commit" + "___" + str(commit.hash) + "___between_commits" + commit_i + "___"
-            + commit_j,
-            commit)
-        output_path = "output/beewteen_commits/" + commit_i + "___" + commit_j + "/"
-        run_code_smile(project, output_path)
-        os.rename(new_project_name, project)
+        commit_path = path_without_release + "/internal_commits/" + "between_commits___from" + commit_i_without_proj_name + "___to" + commit_j_without_proj_name + "/" + commit.hash + "/"
+        if not os.path.exists(commit_path):
+            os.makedirs(commit_path)
+            checkout_to_commit(base_dir + project, commit)
+            run_code_smile(base_dir + project, commit_path)
 
 
 def reorganize_output_code_smile(code_smile_dir_out):
-    list_projects = os.listdir(code_smile_dir_out)
-    for project in list_projects:
-        parent_dir, commit = project.split('___')[0], project.split('___')[1]
-        os.makedirs(code_smile_dir_out + parent_dir + "/", exist_ok=True)
-        shutil.move(code_smile_dir_out + project, code_smile_dir_out + parent_dir + "/" + commit)
+    list_directory = get_all_directory_projects(code_smile_dir_out)
+    for project in list_directory:
+        sub_dirs = get_all_directory_projects(code_smile_dir_out + "/" + project)
+        if not os.path.exists(code_smile_dir_out + "/" + project + "/releases/"):
+            os.makedirs(code_smile_dir_out + "/" + project + "/releases/")
+        for sub_dir in sub_dirs:
+            os.rename(code_smile_dir_out + "/" + project + "/" + sub_dir,
+                      code_smile_dir_out + "/" + project + "/releases/" + sub_dir)
 
 
 def get_project_name(project):
-    return project.split('/')[-1]
+    return project.split("/")[-2]
 
 
 def check_and_save_differences(input_directory, project):
     project_name = get_project_name(project)
-    if not os.path.exists("differences/"):
-        os.makedirs("differences/")
-
+    differences_directory = "differences/" + project_name + "/"
+    if not os.path.exists(differences_directory):
+        os.makedirs(differences_directory)
+    if not os.path.exists(project):
+        os.makedirs(project)
     list_of_dirs = get_all_directory_projects(project)
     for i in range(len(list_of_dirs) - 1):
         commit_i = list_of_dirs[i]
         commit_j = list_of_dirs[i + 1]
         commit_i_only_sha = get_hash_from_project_name(commit_i)
         commit_j_only_sha = get_hash_from_project_name(commit_j)
-        df_i = pd.read_csv(project + commit_i + "/to_save.csv")
-        df_j = pd.read_csv(project + commit_j + "/to_save.csv")
+        df_i = pd.read_csv(os.path.join(project, commit_i, "to_save.csv"))
+        df_j = pd.read_csv(os.path.join(project, commit_j, "to_save.csv"))
         df_only_i = df_i[~df_i.isin(df_j)].dropna()
         df_only_j = df_j[~df_j.isin(df_i)].dropna()
         df_only_i['commit'] = commit_i
@@ -128,26 +125,64 @@ def check_and_save_differences(input_directory, project):
             df_only_i['smelliness___' + commit_i + "___" + commit_j] = 'Stable'
             df_only_j['smelliness___' + commit_i + "___" + commit_j] = 'Stable'
 
-        if not os.path.exists("differences/" + project_name + "/"):
-            os.makedirs("differences/" + project_name + "/")
-        df_only_i.to_csv("differences/" + project_name + "/" + commit_i + "___" + commit_j + ".csv", index=False)
-        df_only_j.to_csv("differences/" + project_name + "/" + commit_j + "___" + commit_i + ".csv", index=False)
+        df_only_i.to_csv(os.path.join(differences_directory, commit_i + "___" + commit_j + ".csv"), index=False)
+        df_only_j.to_csv(os.path.join(differences_directory, commit_j + "___" + commit_i + ".csv"), index=False)
+
+
+def estimate_smelliness_between_two_stable_versions(df1, df2):
+    df1_dropped = df1.drop(columns=['filename'])
+    df2_dropped = df2.drop(columns=['filename'])
+    if df1_dropped.equals(df2_dropped):
+        return "stable"
+    else:
+        if df1["smell"].sum() > df2["smell"].sum():
+            return "increase"
+        elif df1["smell"].sum() < df2["smell"].sum():
+            return "decrease"
+        else:
+            return "different_smells_but_same_smelliness"
 
 
 def start_analysis(project_path, dir):
+    print("*******************Starting analysis of project " + dir + "*******************")
     output_dir_code_smile = "code_smile/output/projects_analysis/" + dir + "/"
-    print(project_path)
     output_dir_code_smile = os.path.abspath(output_dir_code_smile) + "/"
     list_of_releases = get_list_of_releases(project_path)
     for release in list_of_releases:
         project_path_and_hash = project_path + "___" + release.hash
         os.rename(project_path, project_path_and_hash)
-        checkout_to_commit(project_path_and_hash, release)
-        dir_output = output_dir_code_smile + dir + "___" + release.hash
-        run_code_smile(project_path_and_hash, dir_output)
-        os.rename(project_path + "___" + release.hash, project_path)
-        print("Release " + release.hash + " analyzed")
-    check_and_save_differences(project_path, output_dir_code_smile)
+        if checkout_to_commit(project_path_and_hash, release):
+            dir_output = output_dir_code_smile + dir + "___" + release.hash
+            run_code_smile(project_path_and_hash, dir_output)
+            os.rename(project_path + "___" + release.hash, project_path)
+            print("Release " + release.hash + " analyzed")
+        else:
+            print("Release " + release.hash + " not analyzed")
+            os.rename(project_path + "___" + release.hash, project_path)
+
+    # check_and_save_differences(project_path, output_dir_code_smile)
+    reorganize_output_code_smile(
+        "/Users/broke31/PycharmProjects/wrapper_code_smile/code_smile/output/projects_analysis")
+    verify_differencies(
+        "/Users/broke31/PycharmProjects/wrapper_code_smile/code_smile/output/projects_analysis", base_dir)
+
+    print("*******************Analysis of project " + dir + " finished*******************")
+
+
+def verify_differencies(input_projects_analysis, base_dir):
+    projects = get_all_directory_projects(input_projects_analysis)
+    for i in range(len(projects)):
+        list_releases = get_all_directory_projects(input_projects_analysis + "/" + projects[i] + "/releases")
+        for j in range(len(list_releases) - 1):
+            df_i = pd.read_csv(
+                input_projects_analysis + "/" + projects[i] + "/releases/" + list_releases[j] + "/to_save.csv")
+            df_j = pd.read_csv(
+                input_projects_analysis + "/" + projects[i] + "/releases/" + list_releases[j + 1] + "/to_save.csv")
+            if estimate_smelliness_between_two_stable_versions(df_i, df_j) != "stable":
+                print("Project: " + projects[i] + " Release: " + list_releases[j] + " and " + list_releases[
+                    j + 1] + " are different")
+                analyze_commit_by_commit(input_projects_analysis + "/" + projects[i] + "/releases", list_releases[j],
+                                         list_releases[j + 1], projects[i], base_dir)
 
 
 if __name__ == '__main__':
